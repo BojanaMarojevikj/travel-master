18f12fc0229c308ee44899a91d894dbfc26d007e
bojana.marojevikj <bojana@iwmnetwork.com>
2024-07-09 13:51:27 +0200

    display temperature icons

--- app/generate-itinerary/components/ExampleCards.tsx
+++ app/generate-itinerary/components/ExampleCards.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Itinerary } from '../../model/models';
+import { WbSunny, Cloud, AcUnit } from '@mui/icons-material'; 
 
 const ExampleCards: React.FC = () => {
   const exampleItineraries: Itinerary[] = [
@@ -14,11 +15,24 @@ const ExampleCards: React.FC = () => {
       temperature: '22°C',
     },
     {
-        day: 3,
-        activities: ['Visit the Eiffel Tower', 'Lunch at a Parisian café', 'Evening Seine River Cruise'],
-        temperature: '25°C',
-      },
+      day: 3,
+      activities: ['Explore Versailles Palace', 'Shopping in Champs-Élysées', 'Enjoy a French pastry'],
+      temperature: '15°C',
+    },
   ];
+
+  const getTemperatureIcon = (temperature: string) => {
+    const numericTemperature = parseInt(temperature);
+    if (numericTemperature >= 20) {
+      return <WbSunny />; 
+    } 
+    else if(numericTemperature < 20 && numericTemperature > 10){
+      return <Cloud />
+    }
+    else {
+      return <AcUnit />; 
+    }
+  };
 
   return (
     <div className="pt-8">
@@ -27,9 +41,12 @@ const ExampleCards: React.FC = () => {
       </h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 mb-6">
         {exampleItineraries.map((itinerary, index) => (
-          <div key={index} className="bg-white shadow-md rounded-lg p-4">
+          <div key={index} className="bg-[#E3F1FF] shadow-md rounded-lg p-4">
             <h4 className="text-lg font-semibold mb-2">Day {itinerary.day}</h4>
-            <p className="mb-2">Temperature: {itinerary.temperature}</p>
+            <p className="flex items-center mb-2">
+              {getTemperatureIcon(itinerary.temperature)}
+              <span className="ml-2">Temperature: {itinerary.temperature}</span>
+            </p>
             <ul className="list-disc pl-5">
               {itinerary.activities.map((activity, idx) => (
                 <li key={idx}>{activity}</li>
