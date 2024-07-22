b4a83394fa1204a7adc633703a249983a0cbcaf1
BojanaMarojevic <bojana.marojevikj@students.finki.ukim.mk>
2024-07-22 18:36:45 +0200

    added dropdown menu

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,9 @@
+"use client"
 import Image from "next/image";
 import Logo from "../../public/assets/logo.png";
-import User from "../../public/assets/User.svg";
 import Menu from "../../public/assets/Menu.svg";
 import Link from "next/link";
+import { useState } from "react";
 
 const navLinks = [
   { name: "Home", path: "/" },
@@ -11,6 +12,12 @@ const navLinks = [
 ];
 
 export function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="flex bg-blue-100 justify-center items-center">
       <nav className="flex w-full items-center justify-between px-[20px] py-[16px] lg:container lg:px-20 bg-blue-100">
@@ -33,16 +40,33 @@ export function Navbar() {
           </Link>
 
           <div className="flex items-center gap-x-2">
-            <Image src={User} alt="User Profile" />
             <Link href="/sign-in" className="hidden font-medium text-[#36485C] lg:block hover:text-blue-500">
-            <p className="hover:shadow-none hover:border-b-5 hover:border-blue-500 hover:font-semibold">Sign In</p>
+              <p className="hover:shadow-none hover:border-b-5 hover:border-blue-500 hover:font-semibold">Sign In</p>
             </Link>
           </div>
 
-          <Image src={Menu} alt="Menu Button" className="lg:hidden" />
+          <div className="lg:hidden relative">
+            <button onClick={toggleMenu}>
+              <Image src={Menu} alt="Menu Button" />
+            </button>
+            {isMenuOpen && (
+              <div className="absolute right-0 mt-2 bg-white shadow-lg rounded-md w-48">
+                {navLinks.map((item, index) => (
+                  <Link href={item.path} key={index} className="block px-4 py-2 text-[#36485C] hover:bg-blue-500 hover:text-white">
+                    {item.name}
+                  </Link>
+                ))}
+                <Link href="/register" className="block px-4 py-2 text-[#36485C] hover:bg-blue-500 hover:text-white">
+                  Register
+                </Link>
+                <Link href="/sign-in" className="block px-4 py-2 text-[#36485C] hover:bg-blue-500 hover:text-white">
+                  Sign In
+                </Link>
+              </div>
+            )}
+          </div>
         </div>
       </nav>
     </div>
-
   );
 }
